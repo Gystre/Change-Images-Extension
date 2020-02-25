45d78ef46bc41f32c85e44269cd38415bead146b
Kyle Yu <gystre@gmail.com>
2020-02-25 10:21:55 -0800

    made it onload

--- content.js
+++ content.js
@@ -1,4 +1,6 @@
-var imgs = document.getElementsByTagName("img");
-for(var i = 0; i < imgs.length; i++){
-    imgs[i].src = "https://gystre.github.io/assets/profile.jpg";
-}
+window.addEventListener("load", function(){
+    var imgs = document.getElementsByTagName("img");
+    for(var i = 0; i < imgs.length; i++){
+        imgs[i].src = "https://gystre.github.io/assets/profile.jpg";
+    }
+});
